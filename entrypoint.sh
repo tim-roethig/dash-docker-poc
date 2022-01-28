2df11beacbe22a2f769c654d22a1bf4b7d64ef28
Igor Drobot <i.drobot@i-lab.online>
2022-01-28 15:56:58 +0100

    Created start point for the app

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,1 +1,21 @@
-echo "Booooooo"
+#!/bin/bash
+# mailto: ext.igor.drobot@nordlb.de
+# Docker container main point of entry
+
+echo "$(date +'%H:%M:%S %d-%m-%Y') INFO: Entering the entrypoint.sh"
+echo "$(date +'%H:%M:%S %d-%m-%Y') INFO: Passed stage name: $STAGE"
+
+# Check for the STAGE
+if [[ -z ${STAGE} ]]; then
+    echo "$(date +'%H:%M:%S %d-%m-%Y') ERROR: STAGE is not defined!"
+    exit 1
+fi
+
+# Check for the REST_PORT
+if [[ -z ${REST_PORT} ]]; then
+    echo "$(date +'%H:%M:%S %d-%m-%Y') ERROR: REST_PORT is not defined!"
+    exit 1
+fi
+
+python3 app.py
+echo "$(date +'%H:%M:%S %d-%m-%Y') INFO: Completed the execution of entrypoint.sh"
